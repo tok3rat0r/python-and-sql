92657c30d9357e2d62983b5acf98b46b42981428
Owen Jones <owenmjones1@gmail.com>
2021-10-05 21:40:54 +0200

    Modify app to use Poll and Option classes

--- pollApp/app.py
+++ pollApp/app.py
@@ -1,9 +1,8 @@
-import os
-import psycopg2
-from psycopg2.errors import DivisionByZero
-from dotenv import load_dotenv
+from connections import create_connection
+from models.option import Option
+from models.poll import Poll
 import database
-
+import random
 
 DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
 MENU_PROMPT = """-- Menu --
@@ -19,59 +18,59 @@ Enter your choice: """
 NEW_OPTION_PROMPT = "Enter new option text (or leave empty to stop adding options): "
 
 
-def prompt_create_poll(connection):
+def prompt_create_poll():
     poll_title = input("Enter poll title: ")
     poll_owner = input("Enter poll owner: ")
-    options = []
+    poll = Poll(poll_title, poll_owner)
+    poll.save()
 
-    while (new_option := input(NEW_OPTION_PROMPT)):
-        options.append(new_option)
-
-    database.create_poll(connection, poll_title, poll_owner, options)
-
-
-def list_open_polls(connection):
-    polls = database.get_polls(connection)
-
-    for _id, title, owner in polls:
-        print(f"{_id}: {title} (created by {owner})")
+    while new_option := input(NEW_OPTION_PROMPT):
+        poll.add_option(new_option)
 
 
-def prompt_vote_poll(connection):
+def list_open_polls():
+    for poll in Poll.all():
+        print(f"{poll.id}: {poll.title} (created by {poll.owner})")
+
+
+def prompt_vote_poll():
     poll_id = int(input("Enter poll would you like to vote on: "))
 
-    poll_options = database.get_poll_details(connection, poll_id)
-    _print_poll_options(poll_options)
+    _print_poll_options(Poll.get(poll_id).options)
 
     option_id = int(input("Enter option you'd like to vote for: "))
     username = input("Enter the username you'd like to vote as: ")
-    database.add_poll_vote(connection, username, option_id)
+
+    Option.get(option_id).vote(username)
 
 
-def _print_poll_options(poll_with_options) -> list[database.PollWithOption]:
-    for option in poll_with_options:
-        print(f"{option[3]}: {option[4]}")
+def _print_poll_options(options) -> list[Option]:
+    for option in options:
+        print(f"{option.id}: {option.text}")
 
 
-def show_poll_votes(connection):
+def show_poll_votes():
     poll_id = int(input("Enter poll you would like to see votes for: "))
+    poll = Poll.get(poll_id)
+    options = poll.options
+    votes_per_option = [len(option.votes) for option in options]
+    total_votes = sum(votes_per_option)
+
     try:
-        # This gives us count and percentage of votes for each option in a poll
-        poll_and_votes = database.get_poll_and_vote_results(connection, poll_id)
-    except DivisionByZero:
-        print("No votes yet cast for this poll.")
-    else:
-        for _id, option_text, count, percentage in poll_and_votes:
-            print(f"{option_text} got {count} votes ({percentage:.2f}% of total)")
+        for option, votes in zip(options, votes_per_option):
+            percentage = 100.0 * (votes / total_votes)
+            print(f"{option.text} got {votes} votes ({percentage}% of the total).")
+    except ZeroDivisionError:
+        print("No votes cast for this poll yet.")
 
 
-def randomize_poll_winner(connection):
+def randomize_poll_winner():
     poll_id = int(input("Enter poll you'd like to pick a winner for: "))
-    poll_options = database.get_poll_details(connection, poll_id)
-    _print_poll_options(poll_options)
+    _print_poll_options(Poll.get(poll_id).options)
 
     option_id = int(input("Enter which is the winning option, we'll pick a random winner from voters: "))
-    winner = database.get_random_poll_vote(connection, option_id)
+    votes = Option.get(option_id).votes
+    winner = random.choice(votes)
     print(f"The randomly selected winner is {winner[0]}.")
 
 
@@ -85,17 +84,12 @@ MENU_OPTIONS = {
 
 
 def menu():
-    database_uri = input(DATABASE_PROMPT)
-    if not database_uri:
-        load_dotenv()
-        database_uri = os.environ["DATABASE_URI"]
-
-    connection = psycopg2.connect(database_uri)
+    connection = create_connection()
     database.create_tables(connection)
 
     while (selection := input(MENU_PROMPT)) != "6":
         try:
-            MENU_OPTIONS[selection](connection)
+            MENU_OPTIONS[selection]()
         except KeyError:
             print("Invalid input selected. Please try again.")
 
--- pollApp/database.py
+++ pollApp/database.py
@@ -1,9 +1,8 @@
 from psycopg2.extras import execute_values
 
 Poll = tuple[int, str, str]
+Option = tuple[int, str, int]
 Vote = tuple[str, int]
-PollWithOption = tuple[int, str, str, int, str, int]
-PollResults = tuple[int, str, int, float]
 
 CREATE_POLLS = """CREATE TABLE IF NOT EXISTS polls
 (id SERIAL PRIMARY KEY, title TEXT, owner_username TEXT);"""
@@ -12,14 +11,11 @@ CREATE_OPTIONS = """CREATE TABLE IF NOT EXISTS options
 CREATE_VOTES = """CREATE TABLE IF NOT EXISTS votes
 (username TEXT, option_id INTEGER);"""
 
-
+SELECT_POLL = "SELECT * FROM polls WHERE id = %s;"
 SELECT_ALL_POLLS = "SELECT * FROM polls;"
-SELECT_POLL_WITH_OPTIONS = """SELECT * FROM polls
-JOIN options ON options.poll_id = polls.id
-WHERE polls.id = %s;"""
+SELECT_POLL_OPTIONS = "SELECT * FROM options WHERE poll_id = %s;"
 SELECT_LATEST_POLL = """
 SELECT * FROM polls
-JOIN options ON options.poll_id = polls.id
 WHERE polls.id = (
     SELECT id FROM polls ORDER BY id DESC LIMIT 1
 );"""
@@ -33,9 +29,12 @@ FROM options
 LEFT JOIN votes ON votes.option_id = options.id
 WHERE options.poll_id = %s
 GROUP BY options.id;"""
-SELECT_RANDOM_VOTE = "SELECT * FROM votes WHERE option_id = %s ORDER BY RANDOM() LIMIT 1;"
+
+SELECT_OPTION = "SELECT * FROM options WHERE id = %s;"
+SELECT_VOTES_FOR_OPTION = "SELECT * FROM votes WHERE option_id = %s;"
 
 INSERT_POLL_RETURN_ID = "INSERT INTO polls (title, owner_username) VALUES (%s, %s) RETURNING id;"
+INSERT_OPTION_RETURN_ID = "INSERT INTO options (option_text, poll_id) VALUES (%s, %s) RETURNING id;"
 INSERT_OPTION = "INSERT INTO options (option_text, poll_id) VALUES %s;"
 INSERT_VOTE = "INSERT INTO votes (username, option_id) VALUES (%s, %s);"
 
@@ -48,6 +47,25 @@ def create_tables(connection):
             cursor.execute(CREATE_VOTES)
 
 
+# -- polls --
+
+
+def create_poll(connection, title: str, owner: str) -> int:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_POLL_RETURN_ID, (title, owner))
+
+            poll_id = cursor.fetchone()[0]
+            return poll_id
+
+
+def get_poll(connection, poll_id: int) -> Poll:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_POLL, (poll_id,))
+            return cursor.fetchone()
+
+
 def get_polls(connection) -> list[Poll]:
     with connection:
         with connection.cursor() as cursor:
@@ -55,43 +73,46 @@ def get_polls(connection) -> list[Poll]:
             return cursor.fetchall()
 
 
-def get_latest_poll(connection) -> list[PollWithOption]:
+def get_latest_poll(connection) -> Poll:
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_LATEST_POLL)
             return cursor.fetchall()
 
 
-def get_poll_details(connection, poll_id: int) -> list[PollWithOption]:
+def get_poll_options(connection, poll_id: int) -> list[Option]:
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(SELECT_POLL_WITH_OPTIONS, (poll_id,))
+            cursor.execute(SELECT_POLL_OPTIONS, (poll_id,))
             return cursor.fetchall()
 
 
-def get_poll_and_vote_results(connection, poll_id: int) -> list[PollResults]:
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(SELECT_POLL_VOTE_DETAILS, (poll_id,))
-            return cursor.fetchall()
+# -- options --
 
 
-def get_random_poll_vote(connection, option_id: int) -> Vote:
+def get_option(connection, option_id: int) -> Option:
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(SELECT_RANDOM_VOTE, (option_id,))
+            cursor.execute(SELECT_OPTION, (option_id,))
             return cursor.fetchone()
 
 
-def create_poll(connection, title: str, owner: str, options: list[str]):
+def add_option(connection, option_text: str, poll_id: int):
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_POLL_RETURN_ID, (title, owner))
+            cursor.execute(INSERT_OPTION_RETURN_ID, (option_text, poll_id))
+            option_id = cursor.fetchone()[0]
+            return option_id
 
-            poll_id = cursor.fetchone()[0]
-            option_values = [(option_text, poll_id) for option_text in options]
 
-            execute_values(cursor, INSERT_OPTION, option_values)
+# -- votes --
+
+
+def get_votes_for_option(connection, option_id: int) -> list[Vote]:
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_VOTES_FOR_OPTION, (option_id,))
+            return cursor.fetchall()
 
 
 def add_poll_vote(connection, username: str, option_id: int):
